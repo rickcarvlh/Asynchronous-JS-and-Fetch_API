45afcafd7090f03503202ce6a2bb2238ec332d3f
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2018-12-26 18:31:03 +0000

    readme file

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 
 Asynchronous JS and Fetch_API
+
+## Notes
+
+    readyState Values
+      0: request not initialized
+      1: server connection established
+      2: request received
+      3: processing request
+      4: request finished and response is ready
+
+    HTTP Statuses
+     200: OK
+     403: FORBIDDEN
+     404: NOT FOUND
---
+++ XHR Object Methods_Working with_text/app.js
@@ -0,0 +1,34 @@
+document.getElementById('button').addEventListener('click', loadData);
+
+
+function loadData() {
+    // Create an XHR Object
+    const xhr = new XMLHttpRequest();
+
+    // Open
+    xhr.open('GET', 'data.txt', true);
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            console.log(this.responseText);
+
+        }
+
+    }
+
+    xhr.send();
+
+    /**readyState Values
+     * 0: request not initialized
+     * 1: server connection established
+     * 2: request received
+     * 3: processing request
+     * 4: request finished and response is ready
+     * 
+     */
+
+    // HTTP Statuses
+    // 200: OK
+    // 403: FORBIDDEN
+    // 404: NOT FOUND
+}
