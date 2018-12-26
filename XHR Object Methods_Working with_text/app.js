0866ab9eb195b2408b91d34363b1f6bedf000daf
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2018-12-26 18:40:02 +0000

    app.js changes

--- XHR Object Methods_Working with_text/app.js
+++ XHR Object Methods_Working with_text/app.js
@@ -8,14 +8,43 @@ function loadData() {
     // Open
     xhr.open('GET', 'data.txt', true);
 
+
+    // console.log('READYSTATE', xhr.readyState);
+
+    // OPTIONAL - Used for spinners / loaders
+    xhr.onprogress = function () {
+        console.log('READYSTATE', xhr.readyState);
+
+    }
+
+
+    //! New stuff
     xhr.onload = function () {
+        console.log('READYSTATE', xhr.readyState);
         if (this.status === 200) {
-            console.log(this.responseText);
+            // console.log(this.responseText);
+            // prints to ui instead of console
+            document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`
 
         }
 
     }
 
+    // ! old stuff
+    // xhr.onreadystatechange = function () {
+    //     console.log('READYSTATE', xhr.readyState);
+    //     if (this.status === 200 && this.readyState === 4) {
+    //         console.log(this.responseText);
+
+    //     }
+    // }
+
+    xhr.onerror = function () {
+        console.log('Request error.....');
+
+    }
+
+    // mandatory
     xhr.send();
 
     /**readyState Values
