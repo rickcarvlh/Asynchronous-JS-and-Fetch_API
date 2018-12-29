e6ae3487951404b9a08b9c90458d629fee9be475
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2018-12-29 18:23:40 +0000

    get JSON button

--- Fetch_API/app.js
+++ Fetch_API/app.js
@@ -1,6 +1,9 @@
 document.getElementById('button1').addEventListener('click', getText);
+// button2
+document.getElementById('button2').addEventListener('click', getJson);
 
 // fetch returns promises
+// ! Get local text file data
 function getText() {
     fetch('text.txt')
         .then(function (res) {
@@ -10,6 +13,34 @@ function getText() {
         })
         .then(function (data) {
             console.log(data);
+            // output the data to the UI
+            document.getElementById('output').innerHTML = data;
+        })
+        // catch error
+        .catch(function (err) {
+            console.log(err);
+
+        });
+}
+
+// ! Get local JSON data
+function getJson() {
+    fetch('posts.json')
+        .then(function (res) {
+            // here we get a promise
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            // output the data to the UI
+            // document.getElementById('output').innerHTML = data;
+
+            // ? array so we loop trought it
+            let output = '';
+            data.forEach(function (post) {
+                output += `<li>${post.title}</li>`
+            });
+            document.getElementById('output').innerHTML = output;
         })
         // catch error
         .catch(function (err) {
