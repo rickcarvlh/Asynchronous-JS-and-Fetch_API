904e94bb8b329108ff05b6d441786e8e5f0dc028
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2018-12-29 18:28:25 +0000

    get functions added

--- Fetch_API/app.js
+++ Fetch_API/app.js
@@ -1,6 +1,8 @@
 document.getElementById('button1').addEventListener('click', getText);
 // button2
 document.getElementById('button2').addEventListener('click', getJson);
+// button3
+document.getElementById('button3').addEventListener('click', getExternal);
 
 // fetch returns promises
 // ! Get local text file data
@@ -39,6 +41,32 @@ function getJson() {
             let output = '';
             data.forEach(function (post) {
                 output += `<li>${post.title}</li>`
+            });
+            document.getElementById('output').innerHTML = output;
+        })
+        // catch error
+        .catch(function (err) {
+            console.log(err);
+
+        });
+}
+
+// ! Get from external API 
+function getExternal() {
+    fetch('https://api.github.com/users')
+        .then(function (res) {
+            // here we get a promise
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            // output the data to the UI
+            // document.getElementById('output').innerHTML = data;
+
+            // ? array so we loop trought it
+            let output = '';
+            data.forEach(function (user) {
+                output += `<li>${user.login}</li>`
             });
             document.getElementById('output').innerHTML = output;
         })
