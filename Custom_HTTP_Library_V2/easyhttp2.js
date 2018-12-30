5595ed5f80dc7ff4c42e6a9e2436fe739ffdb940
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2018-12-30 13:07:50 +0000

    EasyHttpV2 finished

--- Custom_HTTP_Library_V2/easyhttp2.js
+++ Custom_HTTP_Library_V2/easyhttp2.js
@@ -2,7 +2,7 @@
  * EasyHttp Library
  * Library for making HTTP requests
  * 
- * @version 2.0.0
+ * @version 2.0.0 ES6 version
  * @author Ricardo Carvalho
  * @version MIT
  * 
@@ -11,6 +11,7 @@
 class EasyHttp {
     //  Make an HTTP GET Request
     get(url) {
+        // adding promises
         return new Promise((resolve, reject) => {
             fetch(url)
                 .then(res => res.json())
@@ -19,4 +20,59 @@ class EasyHttp {
         });
 
     }
+
+    // Make an HTTP POST Request
+    post(url, data) {
+        // adding promises
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+
+    }
+
+    // Make an HTTP PUT Request
+    put(url, data) {
+        // adding promises
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+
+    }
+
+    // Make an HTTP DELETE Request
+    delete(url) {
+        // adding promises
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-type': 'application/json'
+                    }
+                })
+                .then(res => res.json())
+                .then(() => resolve('Resource Deleted'))
+                .catch(err => reject(err));
+        });
+
+    }
+
+
 }
